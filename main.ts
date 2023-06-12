de8fa44b5373221aecff4496a2d920a897a1eff4
kurtdevocht <kurt.de.vocht@pandora.be>
2023-06-12 15:58:18 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+function schietWeg () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.slide), SoundExpressionPlayMode.InBackground)
+    servos.P0.setAngle(135)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(5000)
+}
+function telAf () {
+    for (let index = 0; index < 6; index++) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.InBackground)
+        basic.showNumber(game.score())
+        basic.pause(300)
+        game.addScore(-1)
+        basic.pause(300)
+    }
+}
+function init () {
+    game.setScore(5)
+    servos.P0.setAngle(45)
+}
+function pinkePijleke () {
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        # # # . .
+        . # # . .
+        . . # . .
+        `)
+    if (input.buttonIsPressed(Button.A)) {
+        return
+    }
+    basic.pause(50)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (input.buttonIsPressed(Button.A)) {
+        return
+    }
+    basic.pause(50)
+}
+basic.forever(function () {
+    init()
+    while (!(input.buttonIsPressed(Button.A))) {
+        pinkePijleke()
+    }
+    telAf()
+    schietWeg()
+})
